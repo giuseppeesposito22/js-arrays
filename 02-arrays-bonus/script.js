bb831f38ec67d2ad45def48e0ebbaabf6156293b
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-03 15:44:38 +0200

    bonus-array ex. 3

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -28,6 +28,13 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Ed") {
+    teachers.splice(i, 1);
+  }
+}
+
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
