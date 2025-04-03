4f58fbaecb77f83b37426d867ef5302db390b4a4
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-03 13:59:24 +0200

    01-arrays ex. 5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,7 +22,8 @@ console.log(firstTeacher);
 console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-
+teachers.push("Vanessa");
+console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
